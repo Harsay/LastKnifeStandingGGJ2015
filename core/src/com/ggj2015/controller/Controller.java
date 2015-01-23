cfa033e06833d4e6ade43c71d94ad7c1a54d759f
Harsay <harsay@harsay.com>
2015-01-24 00:25:01 +0100

    wtf

--- core/src/com/ggj2015/controller/Controller.java
+++ core/src/com/ggj2015/controller/Controller.java
@@ -16,6 +16,11 @@ public class Controller {
 			p.setX(p.getX()+p.getVelX()*delta);
 			p.setY(p.getY()+p.getVelY()*delta);
 			System.out.println(p.getDir());
+			if(p.getX() < 0) p.setX(0);
+			else if(p.getX()+p.getWidth() > level.getWidth()) p.setX(level.getWidth()-p.getWidth());
+			
+			if(p.getY() < 0) p.setY(0);
+			else if(p.getY()+p.getHeight() > level.getHeight()) p.setY(level.getHeight()-p.getHeight());
 		}
 	}
 	
